86f47e696050346ea4f56f8d0aa17519707313fb
bertoldoklinger <bertoldokbrj@gmail.com>
2023-02-03 02:10:47 -0300

    added new logo svg's and changed sidebar cover

--- react-fundamentals/src/components/Sidebar.jsx
+++ react-fundamentals/src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ export function Sidebar() {
     <aside className={styles.sidebar}>
       <img
         className={styles.cover}
-        src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+        src="./assets/logoBertoldoTransparent.svg"
       />
 
       <div className={styles.profile}>
